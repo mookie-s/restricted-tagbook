3a3dc13261440737dd114bfabc4a307dc2af3503
sei-es <enjoywork19ss@gmail.com>
2024-05-04 19:51:24 +0900

    [add] homeページにおける各ノートカードのモーダル表示機能の追加

--- resources/views/components/layouts/base-layout.blade.php
+++ resources/views/components/layouts/base-layout.blade.php
@@ -9,6 +9,10 @@
     <link rel="stylesheet" href="http://unpkg.com/ress/dist/ress.min.css">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('/images/favicon.png') }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
 </head>
 <body>
     <header class="wrapper">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,8 +17,7 @@
         </ul>
     </div>
     <div class="mastered-tags-tab">
-        <ul>
-            |
+        <ul>|
             @foreach($mastered_tags as $mastered_tag)
             <li><a href="/home/{{ $mastered_tag->id }}">🔖{{ $mastered_tag->abbreviation }}</a></li>|
             @endforeach
@@ -34,7 +33,10 @@
             <a class="first-note-button" href="/note">最初のノートを書く</a>
         </div>
     @endif
+    <!-- この$iはモーダルに各ノート内容を表示するために使用 -->
+    <?php $i = 0; ?>
     @foreach($notes as $note)
+    <?php $i++; ?>
     <div class="link-box">
         <div class="note-list">
             <div class="note-list-image">
@@ -56,8 +58,42 @@
                     <p>{{ Str::limit($note->story, '100', '...')}}</p>
                 </div>
             </div>
-            <button type="submit"></button>
+            <button type="button" data-toggle="modal" data-target="#modal-screen<?= $i ?>" data-backdrop="true"></button>
         </div>
     </div>
+
+    <!-- ここからモーダル -->
+    <div class="modal fade" id="modal-screen<?= $i ?>" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <!-- <h4 class="modal-title" id="myModalLabel">削除確認画面</h4> -->
+                    <p class="modal-title" id="myModalLabel">🔖{{ $note->tag->tagname }}</p>
+                </div>
+                <div class="modal-body">
+                    @if($note->image)
+                    <img src="{{ Storage::url($note->image) }}" alt="{{ $note->title }}">
+                    @else
+                    <!-- <img src="{{ asset('/images/note-image-tag5.png') }}" alt="no-image"> -->
+                    @endif
+                </div>
+                <div class="modal-detail">
+                    <h3 class="modal-title" id="myModalLabel">📝 {{ $note->title }}</h3>
+                    <p>{{ $note->story }}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+                    <!-- <button type="button" class="btn btn-danger">削除</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ここまでモーダル -->
     @endforeach
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </x-layouts.base-layout>
